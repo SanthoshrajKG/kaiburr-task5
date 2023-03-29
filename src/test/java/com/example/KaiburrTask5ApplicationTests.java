d6db45c90fb0d67f4bf0c24a250d56cf6dcad5c5
Santhoshraj KG <santhosh.gvelu@gmail.com>
2023-03-29 20:41:40 +0530

    first commit

---
+++ src/test/java/com/example/KaiburrTask5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaiburrTask5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
